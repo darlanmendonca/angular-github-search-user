83223fa402578b686fa2e40aeafcf7f820faf6e9
Darlan Mendonca <darlanmendonca@gmail.com>
2018-07-25 16:12:19 -0300

    add route to search page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+import { SearchComponent } from './search/search.component';
+
+const routes: Routes = [
+  {path: '', component: SearchComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent],
